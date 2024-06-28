642266b2e2d18ec4ff6b730d84854feda716bd16
vasilisalda0528 <vasilisalda0528@outlook.com>
2024-06-29 01:43:50 +0400

    hide earn wallet buttons and profile then splash page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,27 +8,75 @@ import amar_token from "../assets/images/amarIcon.png";
 import Hide from "../assets/images/logo.png";
 import scroll from "../assets/images/quest.png";
 import Btn from "../components/Btn";
+import profile from "../assets/images/coin.png"
 
 function FirstPage(props) {
   const [holdClick, setHoldClick] = useState(false);
   const [droped, setDroped] = useState(false);
-  let retrieveDrop;
-  let temp, retrieveHold;
+  // const [result, setResult] = useState(false);
+  // const [owner, setOwner] = useState(0);
+  const [choose, setChoose] = useState(false);
+  // const [scores, setScores] = useState(0);
+  const [status, setStatus] = useState({owner:0,hide:false});
+  let owner = 0;
+  let score = parseInt(localStorage.getItem('score'))||0;
+  let result = false;
+  const choosed = (e) => {
+    //  setChoose(true);
+    //  setDroped(false)
+    console.log(e.target.id, owner);
+    if (parseInt(e.target.id) === owner) {
+      score = score + 1;
+      localStorage.setItem('score',score.toString());
+      result = true;
+    } else {
+      result = false;
+    }
+    handleChoose()
+  };
+  const handleChoose = () =>{
+    console.log(score,result)
+    setChoose(true);
+  }
   const static_vases = [
-    <div className="vase-img-small" style={{ left: "90px" }}></div>,
-    <div className="vase-img-big" style={{ left: "175px" }}></div>,
-    <div className="vase-img-small" style={{ left: "270px" }}></div>
+    <div
+      className="vase-img-small"
+      style={{ left: "90px" }}
+      id="1"
+      onClick={choosed}
+    ></div>,
+    <div
+      className="vase-img-big"
+      style={{ left: "175px" }}
+      id="2"
+      onClick={choosed}
+    ></div>,
+    <div
+      className="vase-img-small"
+      style={{ left: "270px" }}
+      id="3"
+      onClick={choosed}
+    ></div>,
   ];
   const dynamic_vases = [
     <div className="vase-img-small" style={{ left: "90px" }} id="cup_1"></div>,
     <div className="vase-img-big" style={{ left: "175px" }} id="cup_2"></div>,
-    <div className="vase-img-small" style={{ left: "270px" }} id="cup_3"></div>
+    <div className="vase-img-small" style={{ left: "270px" }} id="cup_3"></div>,
   ];
+  let retrieveDrop,retOwner;
+  let temp, retrieveHold;
+
+  // const [goEarn, setGoEarn] = useState(false);
   const init = () => {
     return static_vases.map((vase) => vase);
   };
   const move = () => {
     return dynamic_vases.map((vase) => vase);
+  };
+  
+  const hideClick = () => {
+    owner = Math.floor(Math.random(0, 1) * 3 + 1);
+    setStatus({owner,hide:true});
   };
   const handle = () => {
     if (droped) {
@@ -43,28 +91,51 @@ function FirstPage(props) {
     }
     return temp;
   };
+ 
+  const animation = () => {
+    if(status.owner) return "coin_down 2s backwards"
+    else if(result&&choose) return "coin_catch 2s backwards"
+    else if(!result&&choose) return "coin_lost 2s backwards"
+    else return ""
+  }
 
-  useEffect(() => {
-    console.log("input params:->", props);
-    if (holdClick) retrieveDrop = setTimeout(() => setDroped(true), 2500);
-  }, [holdClick]);
-  useEffect(() => {
-    if (droped) {
-      retrieveHold = setTimeout(() => setHoldClick(false), 1000);
-      retrieveDrop = setTimeout(() => setDroped(false), 1000);
-    }
-  }, [droped]);
+  // useEffect(() => {
+  //   console.log("input params:->", props);
+  //   setChoose(false);
+  //   if (holdClick) retrieveDrop = setTimeout(() => setDroped(true), 2500);
+  // }, [holdClick]);
+  // useEffect(() => {
+  //   if (droped) {
+  //     retrieveHold = setTimeout(() => setHoldClick(false), 1000);
+  //     // retrieveDrop = setTimeout(() => setDroped(false), 1000);
+  //   }
+  // }, [droped]);
+
+  // useEffect(() => {
+  //   retOwner = setTimeout(()=>setOwner(0),1000);
+  // }, [result]);
+  // useEffect(() => {
+  //   if (owner == 0) setResult(false);
+  // }, [owner]);
+
+  // useEffect(()=>{localStorage.setItem('score',`${scores}`)},[scores])
+  // useEffect(() => {
+  //   clearTimeout(retrieveHold, retrieveDrop, retOwner);
+  // }, []);
   return (
     <div className="home">
-      
       <div className="info">
         <div className="info-avatar">
-          <div className="info-avatar-imgbox"></div>
+          <div className="info-avatar-imgbox">
+            <img style={{width:'90%', height:'90%'}} src={profile} />
+          </div>
           <div className="info-avatar-text">Vasili</div>
         </div>
         <div className="info-quest">
-          
-          <div className="info-quest-text" style={{position:'relative'}}><img className="info-quest-img" src={scroll} />Quest:Find the hidden $AMAR token</div>
+          <div className="info-quest-text" style={{ position: "relative" }}>
+            <img className="info-quest-img" src={scroll} />
+            Quest:Find the hidden $AMAR token
+          </div>
         </div>
       </div>
       <div className="mask">
@@ -72,28 +143,39 @@ function FirstPage(props) {
           <Btn title="Earn" url="/earn" />
           <div className="panel-score">
             <img src={amar_token} className="panel-score-img" />
-            <div className="panel-score-text">4.999.999</div>
+            <div className="panel-score-text">{score}</div>
           </div>
-          <Btn title="Wallet" url="/wallet"/>
+          <Btn title="Wallet" url="/wallet" />
         </div>
         <div className="hide">
-          <img className="hide-img" src={Hide} onClick={!holdClick ? () => setHoldClick(true) : null} />
+          <img
+            className="hide-img"
+            src={Hide}
+            onClick={!holdClick ? hideClick : null}
+          />
         </div>
         <div className="vase-coin">
-          <div className="coin" style={{ animation: holdClick ? "coin_down 2s backwards" : "" }}>
+          <div
+            className="coin"
+            style={{
+              animation:animation()
+                // (holdClick ? "coin_down 2s backwards" : "") ||
+                // (result
+                //   ? "coin_catch 2s backwards"
+                //   : choose
+                //   ? "coin_lost 2s backwards"
+                //   : ""),
+            }}
+          >
             <div id="coin"></div>
           </div>
-          <div className="vase">
-          {droped ? handle() : init()}
-          </div>
+          <div className="vase">{droped ? handle() : init()}</div>
         </div>
-        
       </div>
       <div className="background">
         {/* <img className="human" src={human}/>
         <img className='dog' src={dog}/> */}
       </div>
-      
     </div>
   );
 }
--- client/src/pages/Splash/splash.js
+++ client/src/pages/Splash/splash.js
@@ -1,9 +1,22 @@
 import './styles.css'
+import React from 'react';
+// import { TiSocialFacebook, TiSocialTwitterCircular, FaInstagram } from 'react-icons/ti';
+import telegramIcon from '../../assets/images/telegramIcon.png'
+import xIcon from '../../assets/images/xIcon.png'
+import yutubeIcon from '../../assets/images/youtubeIcon.png'
 
 const Splash = () =>{
     return <div className="splash">
-        <div className='splash-logo'></div>
-    </div>
+            <div className='splash-logo'></div>
+            <div className="loading-page">
+                <div className="loading-animation"></div>
+                <div className="social-icons">
+                    <img src={telegramIcon}/>
+                    <img src={xIcon}/>
+                    <img src={yutubeIcon}/>
+                </div>
+            </div>
+        </div>
 }
 
 
