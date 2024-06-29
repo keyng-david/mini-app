bd5f74314a284d5cbb4dc22b19063c3699b95919
cryptodoublewang <wkate387729@gmail.com>
2024-06-28 22:41:34 -0400

    add tma sdk

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SDKProvider } from "@tma.js/sdk-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SDKProvider acceptCustomStyles debug>
+      <App />
+    </SDKProvider>
   </React.StrictMode>
 );
 
