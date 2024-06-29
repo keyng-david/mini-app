bd7923a86db9ccde646e0b86e76b5297e7d397a8
vasilisalda0528 <vasilisalda0528@outlook.com>
2024-06-29 18:23:37 +0400

    disable click func until vase selected

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,7 @@ import Hide from "../assets/images/logo.png";
 import scroll from "../assets/images/quest.png";
 import Btn from "../components/Btn";
 import profile from "../assets/images/profile.png"
-
+const log =console.log;
 function FirstPage(props) {
   const [holdClick, setHoldClick] = useState(false);
   const [droped, setDroped] = useState(false);
@@ -17,15 +17,12 @@ function FirstPage(props) {
   const [owner, setOwner] = useState(0);
   const [choose, setChoose] = useState(false);
   // const [scores, setScores] = useState(0);
-  const [status, setStatus] = useState({owner:0,holdClick:false});
+  // const [status, setStatus] = useState({owner:0,holdClick:false});
   // let owner = 0;
   let score = parseInt(localStorage.getItem('score'))||0;
   // let result = false;
   const choosed = (e) => {
-    // setHoldClick(false)
-    // setStatus({...status,holdClick:false})
-
-     
+     setChoose(true)
     console.log(e.target.id, owner);
     if (parseInt(e.target.id) === owner) {
       score = score + 1;
@@ -62,25 +59,16 @@ function FirstPage(props) {
   ];
   let retrieveDrop,retOwner;
   let temp, retrieveHold;
-
-  // const [goEarn, setGoEarn] = useState(false);
   const init = () => {
     return static_vases.map((vase) => vase);
   };
   const move = () => {
     return dynamic_vases.map((vase) => vase);
   };
-  
-  // const hideClick = () => {
-  //   owner = Math.floor(Math.random(0, 1) * 3 + 1);
-  //   setStatus({owner,hide:true});
-  // };
   const handle = () => {
     if (droped) {
       clearTimeout(retrieveDrop);
       temp = move();
-      // retrieveHold = setTimeout(() => setStatus({...status,holdClick:false}), 1000);
-      retrieveHold = setTimeout(() => setHoldClick(false), 1000);
 
       retrieveDrop = setTimeout(() => setDroped(false), 1000);
     } else {
@@ -98,7 +86,7 @@ function FirstPage(props) {
     else return ""
   }
   useEffect(() => {
-      console.log("input params:->", props);
+      console.log("holdclick");
       setChoose(false);
       if (holdClick)retrieveDrop = setTimeout(() => setDroped(true), 2500);
       else setDroped(false)
@@ -107,21 +95,27 @@ function FirstPage(props) {
 
   
 
-  useEffect(() => {
-    setChoose(true);
+  // useEffect(() => {
+  //   console.log('result')
+    
      
-    // retOwner = setTimeout(()=>setOwner(0),1000);
-  }, [result]);
+  //   // retOwner = setTimeout(()=>setOwner(0),1000);
+  // }, [result]);
   useEffect(() => {
     if (owner == 0) setResult(false);
   }, [owner]);
-  useEffect(()=>{if(choose)setDroped(false);},[choose])
   useEffect(() => {
-      if (choose&&!droped) {
-        setHoldClick(false);
+    log('droped',{choose,droped})
+    if (!droped&&choose) {
+      setHoldClick(false);
       }
-  }, [droped]);
+      }, [droped,choose]);
+  useEffect(()=>{
+    log('choose')
+    if(choose)setDroped(false);
 
+  },[choose])
+      
   // useEffect(()=>{localStorage.setItem('score',`${scores}`)},[scores])
   useEffect(() => {
     clearTimeout(retrieveHold, retrieveDrop, retOwner);
@@ -131,14 +125,14 @@ function FirstPage(props) {
       <div className="info">
         <div className="info-avatar">
           <div className="info-avatar-imgbox">
-            <img style={{width:'90%', height:'90%'}} src={profile} />
+            <img style={{width:'90%', height:'90%'}} src={profile} alt="no profile" />
             {console.log({holdClick})}
           </div>
           <div className="info-avatar-text">Vasili</div>
         </div>
         <div className="info-quest">
           <div className="info-quest-text" style={{ position: "relative" }}>
-            <img className="info-quest-img" src={scroll} />
+            <img className="info-quest-img" src={scroll} alt="no quest"/>
             Quest:Find the hidden $AMAR token
           </div>
         </div>
@@ -147,7 +141,7 @@ function FirstPage(props) {
         <div className="panel">
           <Btn title="Earn" url="/earn" />
           <div className="panel-score">
-            <img src={amar_token} className="panel-score-img" />
+            <img src={amar_token} className="panel-score-img" alt="no img"/>
             <div className="panel-score-text">
               444{/* {score} */}
               </div>
@@ -159,6 +153,7 @@ function FirstPage(props) {
             className="hide-img"
             src={Hide}
             onClick={()=>setHoldClick(true) }
+            alt="no img"
           />
         </div>
         <div className="vase-coin">
