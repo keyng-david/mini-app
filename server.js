f6090db8a4f266a863c31e2f91988a6c38482d27
cryptodoublewang <wkate387729@gmail.com>
2024-07-01 16:25:25 -0400

    play, CU user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-const helmet = require('helmet');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const helmet = require("helmet");
 
 const app = express();
 
@@ -13,16 +13,17 @@ app.use(express.json({ extended: false }));
 app.use(helmet());
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/play", require("./routes/api/play"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
